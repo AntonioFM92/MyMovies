b2b115c2dce2f5aadb2d16eead3e63a1b53ee188
AntonioFM <>
2019-08-26 23:07:54 +0200

    Organización del proyecto para usar el patrón MVP con los controladores agrupados

---
+++ MyMovie/Repository/ApiManager.swift
@@ -0,0 +1,9 @@
+//
+//  ApiManager.swift
+//  MyMovie
+//
+//  Created by Antonio Fernández Martín on 26/08/2019.
+//  Copyright © 2019 Antonio Fernández Martín. All rights reserved.
+//
+
+import Foundation
--- MyMovie/View/MovieController.swift
+++ MyMovie/View/MovieController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MovieController.swift
 //  MyMovie
 //
 //  Created by Antonio Fernández Martín on 26/08/2019.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MovieController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
-
